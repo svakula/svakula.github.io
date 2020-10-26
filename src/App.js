4410f6ecb170f906fa6f9c9ee1cce199b41eced9
Shrijith Vakula <svakula31@gmail.com>
2020-10-26 13:28:24 -0700

    Tmp

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+import $ from 'jquery';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
 
-import { BrowserRouter, Route, Link } from 'react-router-dom'; 
-import Projects from './Projects.js'; 
-import Articles from './Articles.js'; 
-import About from './About.js'; 
+class App extends Component {
 
-function App() {
-  return (
-    <BrowserRouter>
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    };
+
+    ReactGA.initialize('UA-110570651-1');
+    ReactGA.pageview(window.location.pathname);
+
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:'./resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
+  render() {
+    return (
       <div className="App">
-  
-        // Set up the Router
-        <Route exact path="/" component={Projects} />
-        <Route path="/articles" component={Articles} />
-        <Route path="/about" component={About} />
-  
-        <div className="navigation">
-          <img src={logo} className="logo-image" alt="Logo Image" />
-          <div className="navigation-sub">
-                                          
-            // Set up the Links
-            <Link to="/" className="item">Projects</Link>
-            <Link to="/articles" className="item">Articles</Link>
-            <Link to="/about" className="item">About</Link>
-  
-          </div>
-        </div>
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        <Contact data={this.state.resumeData.main}/>
+        <Footer data={this.state.resumeData.main}/>
       </div>
-    </BrowserRouter>
-  );
+    );
+  }
 }
 
 export default App;

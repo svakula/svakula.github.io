ba38850389c709bd6ff6c1bfbeaba38f52bf55a6
Shrijith Vakula <svakula31@gmail.com>
2020-10-26 17:36:30 -0700

    Content Update #1

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
+              <li>&copy; Styled and modified by Shrijith Vakula</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
